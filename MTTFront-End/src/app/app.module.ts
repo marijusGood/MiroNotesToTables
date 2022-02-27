69b97e22c247ba61e518e8a7bf52dfd5bb3fd9d7
Marijus Gudiškis <marijus.good@gmail.com>
2022-02-27 17:45:30 +0000

    feat: notes are more reusable

--- MTTFront-End/src/app/app.module.ts
+++ MTTFront-End/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,9 +15,10 @@ import { TablesComponent } from './tables/tables.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, ColorNotesComponent, TablesComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- MTTFront-End/src/app/color-notes/color-notes.component.ts
+++ MTTFront-End/src/app/color-notes/color-notes.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+//TODO: change name to preGenetatingTabelInput
+
 @Component({
-  selector: 'colouredNote',
+  selector: 'preGenetatingTabelInput',
   templateUrl: './color-notes.component.html',
   styleUrls: ['./color-notes.component.css']
 })
 export class ColorNotesComponent implements OnInit {
 
+  listInput: string[] = ["a","a","a","a","a","a","a","a"];
+
+  listOfColors = [
+    "#f5d128",
+    "#fff9b1",
+    "#93d275",
+    "#6cd8fa",
+    "#7b92ff"
+  ];
+
+  listOfFrames = ["Frame 1", "Frame 2"];
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  logList() {
+    console.log(this.listInput[0]);
+    
   }
 
 }
